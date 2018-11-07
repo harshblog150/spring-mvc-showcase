b7ab24a738fe555c5c6b8f729f57e42183d4271b
HSarode-Compumatrice <harshal@compumatrice.com>
2018-11-07 14:28:39 +0530

    first test commit

---
+++ src/main/java/org/springframework/mvc/extensions/ajax/AjaxUtils.java
@@ -0,0 +1,20 @@
+package org.springframework.mvc.extensions.ajax;
+import org.springframework.web.context.request.WebRequest;
+
+public class AjaxUtils {
+
+	public static boolean isAjaxRequest(WebRequest webRequest) {
+		String requestedWith = webRequest.getHeader("X-Requested-With");
+		return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
+	}
+
+	public static boolean isAjaxUploadRequest(WebRequest webRequest) {
+		return webRequest.getParameter("ajaxUpload") != null;
+	}
+	
+	private AjaxUtils()
+	{
+	//Commenting here purposefully!
+	//QA changes final to develop!!!
+	}
+}
